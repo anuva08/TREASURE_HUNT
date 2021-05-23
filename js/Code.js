8a7382e6473b83865b10fb1a52b74f736710d8d2
anuva08 <77385864+anuva08@users.noreply.github.com>
2021-05-23 16:11:28 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,22 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+    fill("white");
+    textSize(15);
+    text("CUTNIFON",700,170);
+    
+    
+    fill("white");
+    textSize(15);
+    text(" ATADASBA",400,280);
+
+}
